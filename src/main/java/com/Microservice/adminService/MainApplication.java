3f853b3b3a2e98788ef73964573f1cacac6a9a2b
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2024-02-28 01:29:05 -0300

    .

--- src/main/java/com/Microservice/adminService/MainApplication.java
+++ src/main/java/com/Microservice/adminService/MainApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAdminServer
+@ComponentScan(basePackages = {"com.Microservice.adminService"})
 public class MainApplication {
 
 	public static void main(String[] args) {
