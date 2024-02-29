5a3710e1920bc00d380289e0ed89180a81759835
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2024-02-29 12:38:22 -0300

    .

---
+++ src/main/java/com/Microservice/adminService/Configuration/AdminServerConfiguration.java
@@ -0,0 +1,10 @@
+package com.Microservice.adminService.Configuration;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AdminServerConfiguration {
+
+}
